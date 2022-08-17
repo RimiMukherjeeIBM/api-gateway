000f6c9f0d8e765da2f0a74858e88f1ed7d519a0
Rimi Mukherjee <107538210+RimiMukherjeeIBM@users.noreply.github.com>
2022-08-17 13:29:12 +0530

    Updated base code

---
+++ src/main/java/com/apig/ApiGatewayApplication.java
@@ -0,0 +1,74 @@
+package com.apig;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.apig.filters.ErrorFilter;
+import com.apig.filters.PostFilter;
+import com.apig.filters.PreFilter;
+import com.apig.filters.RouteFilter;
+import com.apig.jwtfilter.JwtFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+//@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+	    return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+	    return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+	    return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+	    return new RouteFilter();
+	}
+	
+	/**
+	 * Define the bean for Filter registration. Create a new FilterRegistrationBean
+	 * object and use setFilter() method to set new instance of JwtFilter object.
+	 * Also specifies the Url patterns for registration bean.
+	 */
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+    final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+    // Set the filter on this bean
+    registrationBean.setFilter(new JwtFilter("supersecret"));
+    // Set the relevant URL pattern
+    registrationBean.addUrlPatterns("/api/v1/order/*", "/api/v1/user/*");
+    return registrationBean;
+	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+	
+}
---
+++ src/main/java/com/apig/filters/PreFilter.java
@@ -0,0 +1,85 @@
+package com.apig.filters;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class PreFilter extends ZuulFilter{
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		RequestContext ctx = RequestContext.getCurrentContext();
+	    HttpServletRequest request = ctx.getRequest();
+		System.out.println("**Route host***" + ctx.getRouteHost());
+	 /*
+		final String authHeader = request.getHeader("Authorization");
+
+		final Enumeration<String> headers = request.getHeaderNames();
+		
+		System.out.println("********within do filter1***********");
+
+		// We get the Authorization Header of the incoming request
+		
+		System.out.println("********within do filter2***********");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			throw new ZuulException(new Exception(), 401, "Not a valid authentication authHeader");
+		}
+
+		// and retrieve the token
+		String compactJws = authHeader.substring(7);
+
+		try {
+	           final Claims claims = Jwts.parser().setSigningKey("secretkey").parseClaimsJws(compactJws).getBody();
+	            request.setAttribute("claims", claims);
+		} catch (SignatureException ex) {
+		   // HttpServletResponse hsr = new Http;
+		   // hsr.setStatus(401);
+			throw new ZuulException(new Exception(), 401, "Invalid Token");
+		} catch (MalformedJwtException ex) {
+		   // HttpServletResponse hsr = (HttpServletResponse) response;
+		   // hsr.setStatus(401);
+			throw new ZuulException(new Exception(), 401, "JWT is malformed");
+		}*/
+
+	    System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+	    return null;
+	  }
+	
+	private void sendResponse(int responseCode, String responseBody) {
+		final RequestContext ctx = RequestContext.getCurrentContext();
+		ctx.setResponseBody(responseBody);
+		ctx.setResponseStatusCode(responseCode);
+
+
+		try {
+			ctx.getResponse().sendError(responseCode, responseBody);
+		} catch (IOException e) {
+			//log.error(e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+}
